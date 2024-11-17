8becd778d4cbb884b8f1eac0df3b6e58a2f9fad9
BenJR546 <162855613+BenJR546@users.noreply.github.com>
2024-11-17 17:07:58 +1100

    temporary-playersprite

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+        },
+    },
     scene: {
         preload,
         create,
@@ -24,21 +30,33 @@ function preload() {
 function create() {
     console.log("Creating...");
 
-    // Create the tilemap from the loaded JSON key
+    // Map Setup
     const map = this.make.tilemap({ key: "map" });
-
-    // Link the tileset image to the tilemap
     const tileset = map.addTilesetImage("tiles1", "tiles");
-
-    // Create and render the ground layer
     const groundLayer = map.createLayer("Tile Layer 1", tileset, 0, 0);
 
-    // Center the ground layer within the canvas
+    // Center the ground layer
     const offsetX = (config.width - map.widthInPixels) / 2;
     const offsetY = (config.height - map.heightInPixels) / 2;
     groundLayer.setPosition(offsetX, offsetY);
-
     console.log("Ground layer created and centered!");
+
+    // Player Setup
+    const playerSize = 16;
+    this.player = this.add.rectangle(
+        400,
+        300,
+        playerSize,
+        playerSize,
+        0xff0000
+    );
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true);
+    console.log("Player sprite created physics added!");
+    // this.add.text(400, 300, "Hello Phaser!", {
+    //     font: "16px Arial",
+    //     fill: "#ffffff",
+    // });
 }
 
 function update() {
